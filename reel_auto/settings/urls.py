39205bbfe9d8b9c6f866c3ea49bbbd7a38b70d5c
Samangelof <samangelofv@gmail.com>
2025-05-20 19:21:51 +0300

    feat(core): провел боевую настройку проекта

--- reel_auto/settings/urls.py
+++ reel_auto/settings/urls.py
@@ -1,22 +1,13 @@
-"""
-URL configuration for reel_auto project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+# em/settings/urls.py
+"""URL конфигурация проекта"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from settings.config import ADMIN_SITE_URL
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+    path(ADMIN_SITE_URL, admin.site.urls),
+    
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
